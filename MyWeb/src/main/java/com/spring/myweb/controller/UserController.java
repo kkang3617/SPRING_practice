b9ba690ab24862733c91d6d96825a34288ff03e9
kkang3617 <rkdgus3617@naver.com>
2023-05-19 18:12:30 +0900

    DAY64 05.19.

--- MyWeb/src/main/java/com/spring/myweb/controller/UserController.java
+++ MyWeb/src/main/java/com/spring/myweb/controller/UserController.java
@@ -2,13 +2,17 @@ package com.spring.myweb.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.spring.myweb.command.UserVO;
 import com.spring.myweb.user.service.IUserService;
+import com.spring.myweb.util.MailSenderService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,6 +23,9 @@ public class UserController {
 	
 	@Autowired
 	private IUserService service; //서비스에 자동으로 주입해줘~
+	
+	@Autowired
+	private MailSenderService mailService;
 	
 	//회원가입 페이지로 이동
 	@GetMapping("/userJoin")
@@ -40,9 +47,37 @@ public class UserController {
 	@ResponseBody
 	public String mailCheck(String email) {
 		log.info("이메일 인증 요청 들어옴: " + email);
-		
-		return null;
+		return mailService.joinEmail(email); //메일서비스한테 email 보내주고 joinemail해줘~
 	}
+	
+	//회원 가입 처리
+	@PostMapping("/join")
+	public String join(UserVO vo, RedirectAttributes ra) {
+		log.info("param; {}", vo.toString());
+		service.join(vo);
+		ra.addFlashAttribute("msg", "joinSuccess"); //일회성으로 리다이렉트할 때 보내는값
+		return "redirect:/user/userLogin";
+	}
+	
+	//로그인 페이지로 이동 요청
+	@GetMapping("/userLogin")
+	public void login() {}
+	
+	//로그인 요청
+	@PostMapping("/userLogin")
+	public void login(String userId, String userPw, Model model) {
+		log.info("나는 UserController의 login이다~");
+		model.addAttribute("user", service.login(userId, userPw));  //user라는 이름으로 서비스불러서 id pw login에 태우고 모델에 추가
+	}
+	
+	//마이페이지 이동 요청
+	@GetMapping("/userMypage")
+	public void userMypage() {
+		
+	}
+	
+	
+	
 	
 	
 	
--- MyWeb/src/main/java/com/spring/myweb/util/MailSenderService.java
+++ MyWeb/src/main/java/com/spring/myweb/util/MailSenderService.java
@@ -36,7 +36,7 @@ public class MailSenderService {
 		String setFrom = "findemp@naver.com"; //email-config에 설정한 발신용 이메일 주소 입력.
 		String toMail = email; //수신받을 이메일(가입하고자 하는 사람의 이메일)
 		String title = "회원가입 인증 이메일 입니다."; //이메일 제목
-		String content = "홈페이지를 방문해 주셔서 감사합니다." +
+		String content = "홈페이지를 방문해주셔서 감사합니다." +
                 "<br><br>" + 
                 "인증 번호는 <strong>" + authNum + "</strong> 입니다." +
                 "<br>" +
