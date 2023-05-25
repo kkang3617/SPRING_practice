f0b4b0358cf422fb086cba5df19b8529cc43705b
kkang3617 <rkdgus3617@naver.com>
2023-05-25 18:36:07 +0900

    DAY68 05.25.

---
+++ MyWeb/src/main/java/com/spring/myweb/snsboard/mapper/ISnsBoardMapper.java
@@ -0,0 +1,22 @@
+package com.spring.myweb.snsboard.mapper;
+
+import java.util.List;
+
+
+import com.spring.myweb.command.SnsBoardVO;
+import com.spring.myweb.util.PageVO;
+
+public interface ISnsBoardMapper {
+	
+	//등록
+	void insert(SnsBoardVO vo);
+	
+	//목록
+	List<SnsBoardVO> getList(PageVO paging);
+	
+	//상세
+	SnsBoardVO getDetail(int bno); //글 하나 리턴SnsBoardVO
+	
+	//삭제
+	void delte(int bno);
+}
